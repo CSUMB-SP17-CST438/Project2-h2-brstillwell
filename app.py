c7b03590c96e2b195aeffee26cac4fd13c18f891
brstillwell <brstillwell@csumb.edu>
2017-02-19 08:52:54 +0000

    postgresql database

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ users = 0;
 import models 
 
 # URI scheme: postgresql://<username>:<password>@<hostname>:<port>/<database-name>
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/postgres'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 
